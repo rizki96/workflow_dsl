06cf462b600ebeda8794173b93d6f18bd12e6e4c
Iskandar Rizki <iskandar.rizki@gmail.com>
2021-12-20 08:45:53 +0700

    downgrade elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule WorkflowDsl.MixProject do
     [
       app: :workflow_dsl,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
