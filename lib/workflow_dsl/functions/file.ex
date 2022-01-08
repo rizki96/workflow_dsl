6c1e0a4e67bb160e8a36cc78035daf39f742a209
Iskandar Rizki <iskandar.rizki@gmail.com>
2022-01-08 20:24:52 +0700

    add options for file write

--- lib/workflow_dsl/functions/file.ex
+++ lib/workflow_dsl/functions/file.ex
@@ -14,10 +14,33 @@ defmodule WorkflowDsl.File do
     end)
     |> Enum.into(%{})
 
+    options = [:write]
+
+    options =
+    with true <- Map.has_key?(parameters, "utf8") do
+      if parameters["utf8"] == true, do: options ++ [:utf8], else: options
+    else
+      _ -> options
+    end
+
+    options =
+    with true <- Map.has_key?(parameters, "append") do
+      if parameters["append"] == true, do: options ++ [:append], else: options
+    else
+      _ -> options
+    end
+
+    options =
+    with true <- Map.has_key?(parameters, "binary") do
+      if parameters["binary"] == true, do: options ++ [:binary], else: options
+    else
+      _ -> options
+    end
+
     with true <- Map.has_key?(parameters, "input"),
       true <- Map.has_key?(parameters, "output_path"),
       :ok <- File.mkdir_p(Path.dirname(parameters["output_path"])) do
-        File.write(parameters["output_path"], parameters["input"])
+        File.write(parameters["output_path"], parameters["input"], options)
     end
   end
 
