cf01e076c7b946f657a842726dc1790571673efe
iskandar.rizki <iskandar.rizki@rumahsiapkerja.com>
2022-01-23 20:26:41 +0700

    add http delete method, fix args for json

--- lib/workflow_dsl/functions/http.ex
+++ lib/workflow_dsl/functions/http.ex
@@ -40,9 +40,17 @@ defmodule WorkflowDsl.Http do
     request(:put, parameters)
   end
 
-  #def delete(params) do
-  #
-  #end
+  def delete(params) do
+    Logger.log(:debug, "execute :delete, params: #{inspect params}")
+
+    func = Storages.get_last_function_by(%{"module" => __MODULE__, "name" => :delete})
+    parameters = Enum.map(params, fn [k,v] ->
+      {k, Lang.eval(func.session, v)}
+    end)
+    |> Enum.into(%{})
+
+    request(:delete, parameters)
+  end
 
   defp request(:get, %{"url" => url, "headers" => headers} = _params) do
     Req.request(:get, url, headers: headers)
@@ -60,18 +68,18 @@ defmodule WorkflowDsl.Http do
 
   defp request(:post, %{"url" => url, "headers" => headers, "body" => body} = _params) do
     body =
-    cond do
-      is_tuple(body) -> Jason.encode!(Tuple.to_list(body))
-      true -> Jason.encode!(body)
-    end
+      body
+      |> translate_body(:post)
+      |> Jason.encode!()
+    headers =
+      headers |> Enum.map(fn [k, v] -> {k, v} end) |> Enum.into(%{})
     Req.request(:post, url, headers: headers, body: body)
   end
   defp request(:post, %{"url" => url, "body" => body} = _params) do
     body =
-      cond do
-        is_tuple(body) -> Jason.encode!(Tuple.to_list(body))
-        true -> Jason.encode!(body)
-      end
+      body
+      |> translate_body(:post)
+      |> Jason.encode!()
     Req.request(:post, url, body: body)
   end
   defp request(:post, params) do
@@ -85,18 +93,18 @@ defmodule WorkflowDsl.Http do
 
   defp request(:put, %{"url" => url, "headers" => headers, "body" => body} = _params) do
     body =
-      cond do
-        is_tuple(body) -> Jason.encode!(Tuple.to_list(body))
-        true -> Jason.encode!(body)
-      end
+      body
+      |> translate_body(:put)
+      |> Jason.encode!()
+    headers =
+      headers |> Enum.map(fn [k, v] -> {k, v} end) |> Enum.into(%{})
     Req.request(:put, url, headers: headers, body: body)
   end
   defp request(:put, %{"url" => url, "body" => body} = _params) do
     body =
-      cond do
-        is_tuple(body) -> Jason.encode!(Tuple.to_list(body))
-        true -> Jason.encode!(body)
-      end
+      body
+      |> translate_body(:put)
+      |> Jason.encode!()
     Req.request(:put, url, body: body)
   end
   defp request(:put, params) do
@@ -105,6 +113,47 @@ defmodule WorkflowDsl.Http do
       not Map.has_key?(params, "body") -> {:missingparam, [:body], params}
       not Map.has_key?(params, "url") -> {:missingparam, [:url], params}
       true -> {:unknownparam, [], params}
+    end
+  end
+
+  defp request(:delete, %{"url" => url, "headers" => headers, "body" => body} = _params) do
+    body =
+      body
+      |> translate_body(:delete)
+      |> Jason.encode!()
+    headers =
+      headers |> Enum.map(fn [k, v] -> {k, v} end) |> Enum.into(%{})
+    Req.request(:delete, url, headers: headers, body: body)
+  end
+  defp request(:delete, %{"url" => url, "body" => body} = _params) do
+    body =
+      body
+      |> translate_body(:put)
+      |> Jason.encode!()
+    Req.request(:delete, url, body: body)
+  end
+  defp request(:delete, params) do
+    Logger.log(:debug, "delete unknown params: #{inspect params}")
+    cond do
+      not Map.has_key?(params, "url") -> {:missingparam, [:url], params}
+      true -> {:unknownparam, [], params}
+    end
+  end
+
+  defp translate_body(body, http_method) do
+    func = Storages.get_last_function_by(%{"module" => __MODULE__, "name" => http_method})
+    if is_list(body) do
+      Enum.map(body, fn b ->
+        case b do
+          {k, v} ->
+            {k, translate_body(v, http_method)}
+          [k, v] ->
+            {k, translate_body(v, http_method)}
+          _ -> Lang.eval(func.session, body)
+        end
+      end) |> Enum.into(%{})
+    else
+      Lang.eval(func.session, body)
     end
   end
 
