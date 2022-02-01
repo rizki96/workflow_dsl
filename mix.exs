ba57081313fe76dee19a95d5c2237543ee351767
FathanAkram-app <fathan.a.dev@gmail.com>
2022-02-01 07:09:35 +0700

    remove mailman, portability problem, smtp is deprecated

--- mix.exs
+++ mix.exs
@@ -29,7 +29,6 @@ defmodule WorkflowDsl.MixProject do
       {:etso, "~> 0.1.5"},
       {:nimble_parsec, "~> 1.0"},
       {:nimble_csv, "~> 1.2"},
-      {:mailman, "~> 0.4"},
       {:req, git: "https://github.com/wojtekmach/req.git"},
       {:bypass, "~> 2.1", only: :test},
     ]
